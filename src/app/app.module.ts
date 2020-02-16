2b2e578fe43862c396fc54bcbdd923a4c4419bfc
sagarmaharjan <shagarmaharjan@gmail.com>
2020-02-16 16:15:05 +0545

    created structure for the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, ShoppingListComponent, ShoppingEditComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
